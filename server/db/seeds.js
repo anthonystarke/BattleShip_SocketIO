2d4c79df89b5a76295c60e6f13646e34f44d5e54
Anthony Starke <starke@gmail.com>
2019-04-07 11:37:59 +0100

    changed the dbSeed file,currently empty

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,20 +1,14 @@
-use birds;
+use battleShip;
 db.dropDatabase();
 
-db.sightings.insertMany([
+db.players.insertMany([
   {
-    species: "Yellow Wagtail",
-    location: "Sutherland",
-    date: "2017-06-01"
+
   },
   {
-    species: "Red Kite",
-    location: "Knockshinnoch",
-    date: "2017-01-22"
+
   },
   {
-    species: "Little Egret",
-    location: "Seamill",
-    date: "2018-08-15"
+    
   }
 ]);
